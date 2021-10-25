36fb5dc7f397e2c972727382ef4dce36ac8186fb
devisati2010 <61694423+devisati2010@users.noreply.github.com>
2021-10-25 12:05:19 -0300

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,35 +1,38 @@
 module.exports = (app)=>{
+ 
     var conexao = require("../config/database.js")()
-    var usuarios = require("../models/usuarios")
-    var atividades = require("../models/atividades")
-    
-    app.get('/dashboard', async(req, res)=>{
+    var usuarios = require("../model/usuarios")
+    var atividades = require("../model/atividades")
+ 
+    app.get('/dashboard',async(req,res)=>{
         var id = req.query.id
         console.log(id)
         if(!id){
             res.redirect('/login')
         }else{
-            var userexiste = await usuarios.findOne({_id:id})
-            if(userexiste){
-                res.render('dashboard.ejs',{login:userexiste})
-            }else{
-                res.redirect("/login")
-            }
-        }   
+         var userexiste = await usuarios.findOne({_id:id})
+         if(userexiste){
+         res.render('dashboard.ejs',{login:userexiste})
+         }else{
+          res.redirect('/login')
+        }
+        }
     })
-    app.post('dashboard', async(req,res)=>{
-        var dados = req.body
+    
+    app.post('/dashboard',async(req,res)=>{
+       var dados = req.body
+       
+       var novo = await new atividades({
+           data: dados.data,
+           tipo: dados.tipo,
+           entrega: dados.entrega,
+           disciplina: dados.disciplina,
+           instrucoes: dados.instrucoes,
+           user: dados.id
         
-        var novo = await new atividades({
-            data: dados.data,
-            tipo: dados.tipo,
-            entrega: dados.entrega,
-            diciplina: dados.diciplina,
-            instrucoes: dados.instrucoes,
-            user: dados.id
-        }).save()
-        .then ((result)=>{
-            res.redirect('/dashboard?id='+dados.id)
-        })
+       }).save()
+       .than((result)=>{
+           res.redirect('/dashboard?id='+dados.id)
+       })
     })
 }
